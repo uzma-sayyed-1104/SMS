6cb3cea38a8715596010192fdc2676e838452b6f
Uzma Sayyed <sayyeduzma313@gmail.com>
2025-07-08 21:34:27 +0530

    Corrections

--- my-appp/backend/app.js
+++ my-appp/backend/app.js
@@ -10,9 +10,6 @@ const noticeRoutes = require('./routes/notices');
 const subjectRoutes = require('./routes/subjects');
 const adminRoutes = require('./routes/admin');
 const complaintRoutes = require('./routes/complaints');
-const teacherHomeRoute = require('./routes/teacherHomeRoutes');
-const teacherClassRoutes = require('./routes/teacherClassRoute');
-const teacherlogRoutes = require('./routes/teacherlogin');
 
 
 
@@ -35,9 +32,7 @@ app.use('/api/notices', noticeRoutes);
 app.use('/api/subjects', subjectRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/complaints', complaintRoutes);
-app.use('/api/teacherHomeRoute', teacherHomeRoute);
-app.use('/api/teacherClassRoute', teacherClassRoutes);
-app.use('./api/teacherlogin', teacherlogRoutes);
+
 
 
 // MongoDB Connection
