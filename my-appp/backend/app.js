6496a402c2ca7360b5e5bae8aa756415610b2df4
Uzma Sayyed <sayyeduzma313@gmail.com>
2025-07-15 20:23:47 +0530

    corrections

--- my-appp/backend/app.js
+++ my-appp/backend/app.js
@@ -17,7 +17,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use('*',cors({
+  origin: 'https://sms-kzta.onrender.com', // ✅ Your frontend domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Routes
